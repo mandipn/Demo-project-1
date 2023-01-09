2c470a7532bc6a3b8da1c1d20079abac0b282fd7
David Marsh <40401344+Daveym1@users.noreply.github.com>
2023-01-09 05:38:07 +0000

    added files to first api branch

--- JS/script.js
+++ JS/script.js
@@ -1,69 +1,102 @@
 var ingredientList = $("#ingredientList");
-var nation = "japanese";
-var nationInput = $("#submit > img");
-var submit = $("#submit");
+var nation;
+var nationInput = $("img");
+var dishes = $("#dishes");
 
-// This is the event listener for the flags - it's currently using the id of the submit button and logging the title attribute of the flag image
+// This is the event listener for the flags
+// It's storing the title attribute of the flag in a variable to be used in the first API call
+// and logging the title attribute of the flag image
 
-submit.on("click", function (event) {
-  event.preventDefault();
-  nation = nationInput.attr("title");
-  console.log(nation);
+nationInput.on("click", function () {
+  var flag = $(this);
+  nation = flag.attr("title");
+  console.log(flag);
 
-  // First API call, returns all the meals from the inputted nation which is currently taken from the title attribute of the flag image
+  // First API call, returns all the meals from the clicked nation
+  // Success property calls the mealId function so we can use the data in the second API call
 
   $.ajax({
     url: "https://www.themealdb.com/api/json/v1/1/filter.php?a=" + nation,
     method: "GET",
-    success: mealId, // success calls the mealId function so we can use the data outside of the function
+    success: mealId,
   });
+
+  // This function pushes all the mealIds from the response into an array
 
   function mealId(data) {
     console.log(data);
-    const idMeals = data.meals.map((meal) => meal.idMeal); // this pushes all the mealIds from the response into an array
+    var idMeals = data.meals.map((meal) => meal.idMeal);
 
     console.log(idMeals);
+
+    // This clears the html section before displaying the dishes
+
+    dishes.empty();
+
+    // This appends a card for each dish from the nation
+    // I limited the amount of dishes displayed with the slice method to 12 dishes
+
+    for (var meal of data.meals.slice(0, 12)) {
+      dishes.append(
+        `<section class="meals">` +
+          `<div class="card meal1" style="width: 18rem" data-id="${meal.idMeal}">` +
+          `<img class="card-img-top" src="${meal.strMealThumb}" alt="Card image cap" />` +
+          `<div class="card-body">` +
+          ` <h5 class="card-title">${meal.strMeal}</h5>` +
+          ` </div>` +
+          ` </div>` +
+          `</section>`
+      );
+    }
+
+    // This event listener uses the find method to give us the id data attribute of the clicked card
+    // which is the meal id of the dish which we'll use in the second API call
+
+    dishes.find(".card").click(function () {
+      var id = $(this).data("id");
+
+      // second API call takes the meal id variable and uses it to find the ingredients of the dish then logs the result
+
+      $.ajax({
+        method: "GET",
+        url: "https://www.themealdb.com/api/json/v1/1/lookup.php?i=" + id,
+      }).then(function (response) {
+        console.log(response);
+
+        // Billy's loops
+        // This for-of loop iterates through the meals in the returned object from the last call
+        // It finds all the ingredients and measures and pushes them to new arrays
+
+        let ingredients = [];
+        let measures = [];
+
+        for (let [key, value] of Object.entries(response.meals[0])) {
+          if (key.startsWith("strIngredient") && value !== "") {
+            ingredients.push(value);
+          }
+          if (key.startsWith("strMeasure") && value !== "") {
+            measures.push(value);
+          }
+        }
+
+        console.log(ingredients);
+        console.log(measures);
+
+        // The for loop iterates through the two new arrays,
+        // concantenates the results and pushes them to a new array
+        // giving us the list of ingredients and measures to be used in the call to the second API
+
+        let recipes = [];
+
+        let size = measures.length;
+
+        for (let i = 0; i < size; i++) {
+          let recipe = measures[i].trim() + " " + ingredients[i];
+          recipes.push(recipe);
+        }
+
+        console.log(recipes);
+      });
+    });
   }
 });
-
-
-
-//     // second ajax call takes the result variable and uses it to find the ingredients of the dish then logs the result
-//     // I've concatenated the ingredient measures and the ingredients and logged the result
-
-//     $.ajax({
-//       method: "GET",
-//       url: "https://www.themealdb.com/api/json/v1/1/lookup.php?i=" + result,
-//     }).then(function (response) {
-//       console.log(response);
-
-//       // Billy's loops
-
-//       let ingredients = [];
-//       let measures = [];
-
-//       for (let [key, value] of Object.entries(response.meals[0])) {
-//         if (key.startsWith("strIngredient") && value !== "") {
-//           ingredients.push(value);
-//         }
-//         if (key.startsWith("strMeasure") && value !== "") {
-//           measures.push(value);
-//         }
-//       }
-
-//       console.log(ingredients);
-//       console.log(measures);
-
-//       let recipes = [];
-
-//       let size = measures.length;
-
-//       for (let i = 0; i < size; i++) {
-//         let recipe = measures[i].trim() + " " + ingredients[i];
-//         recipes.push(recipe);
-//       }
-
-//       console.log(recipes);
-
-//   });
-// }
