bed5d7ef9095f888cf8257605df94af559ca4ebc
hibz87 <115959890+hibz87@users.noreply.github.com>
2023-01-12 20:48:47 +0000

    added styles first page

---
+++ project-version-1.0/chart.js
@@ -0,0 +1,63 @@
+import { getNutritionData } from "./storage.js"
+var spanEl = document.querySelector('.title-of-app')
+
+
+
+var x = document.createElement("span");
+x.textContent = ` 2Go`;
+x.style.color ="red";
+
+spanEl.appendChild(x);
+
+
+$("#nav-chart-tab").click(function () {
+	let nutritionData = getNutritionData()
+
+	if (!nutritionData) {
+		$("#nav-chart").empty()
+		$("#nav-chart").append("<p> No data to plot</p>")
+		return
+	}
+
+	let { Calories, FAT, Protein, Carbs } = computePercentage(nutritionData)
+
+	var options = {
+		title: {
+			text: "Calories - " + Calories,
+			fontWeight: "lighter",
+			margin: 2,
+		},
+		width: 350,
+		height: 250,
+		animationEnabled: true,
+		willReadFrequently: true,
+		data: [{
+			type: "pie",
+			startAngle: 40,
+			toolTipContent: "<b>{label}</b>: {y}%",
+			showInLegend: "true",
+			legendText: "{label}",
+			indexLabelFontSize: 15,
+			indexLabel: "{label} - {y}%",
+			dataPoints: [
+				{ y: FAT, label: "FAT" },
+				{ y: Carbs, label: "Carbs" },
+				{ y: Protein, label: "Protein" },
+			]
+		}]
+	};
+
+	$("#nav-chart").CanvasJSChart(options);
+})
+
+
+function computePercentage({ Calories, FAT, Protein, Carbs }) {
+	let totalNutrients = FAT + Protein + Carbs
+
+	return {
+		Calories: Calories,
+		FAT: Number((FAT / totalNutrients) * 100).toFixed(1),
+		Protein: Number((Protein / totalNutrients) * 100).toFixed(1),
+		Carbs: Number((Carbs / totalNutrients) * 100).toFixed(1)
+	}
+}
---
+++ project-version-1.0/script.js
@@ -0,0 +1,35 @@
+import { showMeals } from "./meals.js"
+
+$(document).ready(function () {
+
+    //Make sure the all th other section but the welcome section are hidden from view
+    showWelcomeSection()
+
+    // Return to home page when logo is clicked
+    $(".navbar-brand").click(function (event) {
+        event.preventDefault()
+        window.location.href = window.location.href
+        showWelcomeSection()
+    })
+
+    //First click shows the different meals
+
+    $(".flags col").click(function (event) {
+        event.preventDefault();
+
+        let nationality = event.target.title;
+        // First API call, returns all the meals from the inputted nation
+        $.ajax({
+            url: "https://www.themealdb.com/api/json/v1/1/filter.php?a=" + nationality,
+            method: "GET"
+        }).then(showMeals)
+    })
+
+
+    // hides all the other section but the welcome section
+    function showWelcomeSection() {
+        $(".welcome").show()
+        $(".meals").hide()
+        $(".nutrition").hide()
+    }
+})
